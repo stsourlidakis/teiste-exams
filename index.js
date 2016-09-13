035615d0706abb6d0ccd50b2e9205bb3f358cd1c
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-09-13 21:20:58 +0300

    implement basic upload to imgur

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express'),
 	app = express(),
-	monk = require('monk');
+	monk = require('monk'),
+	multer = require('multer'),
+	bodyParser = require('body-parser'),
+	imgur = require('imgur');
 
 require('dotenv').config();
+
 const  url = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+'/'+process.env.DB_NAME,
 	db = monk(url);
+
+app.use('/public', express.static('public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+var upload = multer({ storage: multer.memoryStorage({}) });
+
+imgur.setClientId(process.env.IMGUR_CLIENTID);
 
 const images = db.get('images');
 
@@ -33,6 +47,30 @@ app.get('/insert/sample',function(req, res){
 		.catch((err) => {
 			res.send(err);
 		});
+});
+
+app.post('/upload', upload.single('image'), function(req, res){
+	const albumId = 'mnVUvevYnrhvxq0';	//inf
+	imgur.uploadBase64(req.file.buffer.toString('base64'), albumId )
+    .then(function (imgurRes) {
+        images.insert({
+			'url': imgurRes.data.link,
+			'deleteUrl': imgurRes.data.deletehash,
+			'tags': req.body.tags.split(','),
+			'reports': 0,
+			'active': true,
+			'uploader': 'anon',
+		})
+		.then((docs)=>{
+			res.json(docs);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+    })
+    .catch(function (err) {
+		res.send ( err.message );
+    });
 });
 
 app.listen(2095, function () {
