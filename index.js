fc2f13cb09ea7a4fe41bd1d72ec69a92181d4447
stsourlidakis <s.tsourlidakis@hotmail.com>
2025-08-14 19:41:33 +0100

    refactor: convert promises to async/await

--- index.js
+++ index.js
@@ -53,33 +53,29 @@ app.locals.PAYPAL_DONATE_ID = process.env.PAYPAL_DONATE_ID;
 app.locals.useCaptchaOnUploads = utils.settings.useCaptchaOnUploads;
 app.locals.serverStarted = new Date().toGMTString();
 
-app.get('/', function (req, res) {
-  semesters
-    .find({}, { sort: { _id: 1 } })
-    .then((docs) => {
-      res.render('home', { semesters: docs });
-    })
-    .catch((err) => {
-      utils.sendToErrorPage(err, req, res);
-    });
+app.get('/', async function (req, res) {
+  try {
+    const docs = await semesters.find({}, { sort: { _id: 1 } });
+    res.render('home', { semesters: docs });
+  } catch (err) {
+    utils.sendToErrorPage(err, req, res);
+  }
 });
 
-app.get('/all', function (req, res) {
-  images
-    .find({ active: true }, { sort: { _id: -1 } })
-    .then((docs) => {
-      const metadata = {
-        courseName: 'Όλα τα μαθήματα',
-        courseKey: '',
-        year: 'Όλα',
-        count: docs.length,
-      };
+app.get('/all', async function (req, res) {
+  try {
+    const docs = await images.find({ active: true }, { sort: { _id: -1 } });
+    const metadata = {
+      courseName: 'Όλα τα μαθήματα',
+      courseKey: '',
+      year: 'Όλα',
+      count: docs.length,
+    };
 
-      res.render('gallery', { meta: metadata, images: docs });
-    })
-    .catch((err) => {
-      utils.sendToErrorPage(err, req, res);
-    });
+    res.render('gallery', { meta: metadata, images: docs });
+  } catch (err) {
+    utils.sendToErrorPage(err, req, res);
+  }
 });
 
 app.get('/about', function (req, res) {
@@ -94,38 +90,36 @@ app
   .post(
     utils.multerUpload.fields([]),
     utils.verifyReCaptchaForContact,
-    function (req, res) {
+    async function (req, res) {
       const data = {
         name: req.body.name,
         email: req.body.email,
         msg: req.body.msg,
       };
 
-      contactMessages
-        .insert(data)
-        .then(() => {
-          res.render('contact', {
-            error: false,
-            resultMessage: 'Το μήνυμα αποθηκεύτηκε',
-          });
-        })
-        .catch((err) => {
-          console.log(
-            'Error while saving contact message: ',
-            data,
-            'error: ',
-            err
-          );
-          res.render('contact', {
-            error: true,
-            resultMessage:
-              'Πρόβλημα κατά την αποθήκευση του μηνύματος, δοκιμάστε ξανά αργότερα',
-          });
+      try {
+        await contactMessages.insert(data);
+        res.render('contact', {
+          error: false,
+          resultMessage: 'Το μήνυμα αποθηκεύτηκε',
         });
+      } catch (err) {
+        console.log(
+          'Error while saving contact message: ',
+          data,
+          'error: ',
+          err
+        );
+        res.render('contact', {
+          error: true,
+          resultMessage:
+            'Πρόβλημα κατά την αποθήκευση του μηνύματος, δοκιμάστε ξανά αργότερα',
+        });
+      }
     }
   );
 
-app.get('/course/:course/year/:year', function (req, res, next) {
+app.get('/course/:course/year/:year', async function (req, res, next) {
   if (
     (utils.courses.keyExists(req.params.course) && req.params.year === 'all') ||
     utils.yearExists(req.params.year)
@@ -135,50 +129,47 @@ app.get('/course/:course/year/:year', function (req, res, next) {
       filter.year = req.params.year;
     }
 
-    images
-      .find(filter, { sort: { year: -1 } })
-      .then((docs) => {
-        const metadata = {
-          courseName: utils.courses.getNameFromKey(req.params.course),
-          courseKey: req.params.course,
-          year: req.params.year !== 'all' ? req.params.year : 'Όλα',
-          count: docs.length,
-        };
+    try {
+      const docs = await images.find(filter, { sort: { year: -1 } });
+      const metadata = {
+        courseName: utils.courses.getNameFromKey(req.params.course),
+        courseKey: req.params.course,
+        year: req.params.year !== 'all' ? req.params.year : 'Όλα',
+        count: docs.length,
+      };
 
-        res.render('gallery', { meta: metadata, images: docs });
-      })
-      .catch((err) => {
-        utils.sendToErrorPage(err, req, res);
-      });
+      res.render('gallery', { meta: metadata, images: docs });
+    } catch (err) {
+      utils.sendToErrorPage(err, req, res);
+    }
   } else {
     next(); //default 404
   }
 });
 
-app.get('/course/:course/exam/:id', function (req, res, next) {
+app.get('/course/:course/exam/:id', async function (req, res, next) {
   if (
     utils.courses.keyExists(req.params.course) &&
     utils.isValidMongoID(req.params.id)
   ) {
-    images
-      .findOne({
+    try {
+      const doc = await images.findOne({
         courseKey: req.params.course,
         _id: req.params.id,
         active: true,
-      })
-      .then((doc) => {
-        if (doc) {
-          const metadata = {
-            courseName: utils.courses.getNameFromKey(doc.courseKey),
-          };
-          res.render('exam', { meta: metadata, image: doc });
-        } else {
-          next(); //default 404
-        }
-      })
-      .catch(() => {
-        next(); //default 404
       });
+
+      if (doc) {
+        const metadata = {
+          courseName: utils.courses.getNameFromKey(doc.courseKey),
+        };
+        res.render('exam', { meta: metadata, image: doc });
+      } else {
+        next(); //default 404
+      }
+    } catch {
+      next(); //default 404
+    }
   } else {
     next(); //default 404
   }
@@ -197,57 +188,55 @@ app
     utils.verifyReCaptchaForUpload,
     utils.checkRequiredInputs,
     utils.checkPhotoContent,
-    function (req, res) {
+    async function (req, res) {
       const albumId = process.env.IMGUR_ALBUM_DELETE_HASH; //delete hash because the album is anonymous
       const courseKey = utils.courses.getKeyFromName(req.body.courseName);
 
-      imgur
-        .upload({
+      try {
+        const imgurRes = await imgur.upload({
           image: req.file.buffer.toString('base64'),
           type: 'base64',
           album: albumId,
-        })
-        .then(function (imgurRes) {
-          const httpsUrl = utils.httpsUrl(imgurRes.data.link);
-
-          return images.insert({
-            url: httpsUrl,
-            thumbnailUrl: thumbnails.medium(httpsUrl),
-            deleteHash: imgurRes.data.deletehash,
-            courseKey: courseKey,
-            year: req.body.year,
-            period: req.body.period,
-            active: utils.settings.defaultImageActive,
-            uploader: utils.settings.defaultUploaderName,
-          });
-        })
-        .then(() => {
-          utils.courseItemIncrease(courseKey, req.body.year);
-          res.render('upload', {
-            error: false,
-            resultMessage: 'Το θέμα αποθηκεύτηκε!',
-            courses: utils.courses.all,
-            years: utils.settings.years,
-          });
-        })
-        .catch(function (err) {
-          if (utils.settings.log500) {
-            utils.log500(err, req);
-          }
-          res.render('upload', {
-            error: true,
-            resultMessage: 'Κάτι πήγε στραβά, παρακαλώ δοκιμάστε ξανά.',
-            courses: utils.courses.all,
-            years: utils.settings.years,
-          });
         });
+
+        const httpsUrl = utils.httpsUrl(imgurRes.data.link);
+
+        await images.insert({
+          url: httpsUrl,
+          thumbnailUrl: thumbnails.medium(httpsUrl),
+          deleteHash: imgurRes.data.deletehash,
+          courseKey: courseKey,
+          year: req.body.year,
+          period: req.body.period,
+          active: utils.settings.defaultImageActive,
+          uploader: utils.settings.defaultUploaderName,
+        });
+
+        utils.courseItemIncrease(courseKey, req.body.year);
+        res.render('upload', {
+          error: false,
+          resultMessage: 'Το θέμα αποθηκεύτηκε!',
+          courses: utils.courses.all,
+          years: utils.settings.years,
+        });
+      } catch (err) {
+        if (utils.settings.log500) {
+          await utils.log500(err, req);
+        }
+        res.render('upload', {
+          error: true,
+          resultMessage: 'Κάτι πήγε στραβά, παρακαλώ δοκιμάστε ξανά.',
+          courses: utils.courses.all,
+          years: utils.settings.years,
+        });
+      }
     }
   );
 
-app.use(function (req, res) {
+app.use(async function (req, res) {
   //default 404
   if (utils.settings.log404 && req.originalUrl != '/favicon.ico') {
-    utils.log404(req);
+    await utils.log404(req);
   }
 
   res.status(404).render('404');
